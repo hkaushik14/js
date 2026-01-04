72aee6af13099e1974d7341da2ea72f97eb87148
Harsh Kaushik <harshkaushik494@gmail.com>
2026-01-04 19:37:40 +0530

    variable,datatype

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "harshkaushik494@gmail.com"
+var accountPassword = "0987"
+accountCity = "Roorkee"
+let accountstate
+
+// accountId = 54321 // not allowed
+accountEmail = "harsh@gmail.com"
+accountPassword = "6789"
+accountCity = "Dehradun"
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountEmail, accountPassword, accountCity, accountstate);
---
+++ 02_datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as modern
+
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3)  // code readiability
+
+let name = "harsh"
+let age = 21
+let isLogg=false 
+//Boolean => true/false
+// null => empty value
+let state; // undefined => value not assigned\
+// symbol => unique identifier
+
+
+//object => key-value pairs
+
+console.log(typeof name);
