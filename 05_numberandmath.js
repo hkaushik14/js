d5f208482040c4409620d62f484246c80e4c29f6
Harsh Kaushik <harshkaushik494@gmail.com>
2026-01-08 23:48:42 +0530

    number and maths

---
+++ 05_numberandmath.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log("score is " + score);
+
+// const balance = new Number(1000)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+
+// const otherNumber = "1234.5678"
+// // console.log(otherNumber.toPrecision(1));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// +++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-100));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(0,150,30,20,-8,-200));
+// console.log(Math.max(0,150,30,20,-8,-200));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1); // 1-10
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()*(max-min+1))+min); // 10-20
