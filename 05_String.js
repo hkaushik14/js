9385942a115f57de0c8a50a71c992943ae658ab8
Harsh Kaushik <harshkaushik494@gmail.com>
2026-01-08 23:35:24 +0530

    String

---
+++ 05_String.js
@@ -0,0 +1,23 @@
+const name ="harsh"
+const greeting ="hello world"
+// console.log(name + " says " + greeting);
+console.log(`${name} says ${greeting}`);
+
+const gameName = new String('harshhic')
+
+console.log(gameName.length);
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+console.log(gameName.includes('hi'));
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('h'));
+console.log(gameName.endsWith('c'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(0,4));
+console.log(gameName.split(''));
+console.log(gameName.replace('hi','hello'));
+
+const url = 'http://harshhic.com/about/us/contact.html'
+console.log(url.split('/'));
+console.log(url.replace('contact.html','home.html'));
+
