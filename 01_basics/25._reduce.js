8055cdad794918f6cf39a46635154a5a394a3b0f
hkaushik14 <harshkaushik494@gmail.com>
2026-01-11 01:22:44 +0530

    reduce

---
+++ 01_basics/25._reduce.js
@@ -0,0 +1,15 @@
+const array1 = [1, 2, 3, 4];
+
+const sum = array1.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum); // Output: 10
+// const mytotal = array1.reduce(function(acc, currentValue){
+//     console.log('Acc: ${acc} and currval: ${currentValue}');
+//     return acc + currentValue;
+// }, 0)
+
+// console.log(mytotal);
+
+
