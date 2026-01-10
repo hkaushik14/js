9b108f2b731fe41584ce66da5eeef85fe880b27e
hkaushik14 <harshkaushik494@gmail.com>
2026-01-11 00:50:23 +0530

    ifelse

---
+++ 01_basics/16_behind the scene.js
@@ -0,0 +1,28 @@
+// -> Execution Phase
+
+// 1 -> Global Execution 
+//         this
+
+// 2  Memory Phase 
+//   val1 -> undefined
+//     val2 -> undefined
+
+
+
+// 3  Execution Phase 
+// vals -> 10
+// val2 -> 5
+// add num ->
+
+// 4  Memory Phase
+// vals -> 10
+// val2 -> 5
+// add num -> 5 
+
+// 5  Execution Phase
+// vals -> 10
+// val2 -> 5
+// add num -> 5 
+// return -> 15
+
+
